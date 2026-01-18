40c728bbe317fe1f0679acb27f7c946d507571af
Mukhiddin <108138410+UMaRzHaN@users.noreply.github.com>
2026-01-19 03:07:43 +0500

    small fixxx

--- src/components/PhotoModal/PhotoModal.jsx
+++ src/components/PhotoModal/PhotoModal.jsx
@@ -9,6 +9,7 @@ export function PhotoModal({ open, item, onClose }) {
           📷 Фото доступно только в мобильном приложении
         </div>
       )}
+      <img src={item.photoSrc} alt="Утечка" />
       <div style={{ color: "#000000", marginTop: 6 }}>
         путь к фото: {String(item.photo)}
       </div>
