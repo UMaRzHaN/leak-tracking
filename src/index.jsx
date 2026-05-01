5459a21c9010b64b83ecb3bdac7c4f123a4aa2b0
Mukhiddin <mmusha221135@gmail.com>
2026-05-01 15:17:52 +0500

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app/App';
-import { ProjectProvider } from './app/project/ProjectContext';
-import { LeakFormProvider } from './features/leakForm/LeakFormContext';
-import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
-import { reportWebVitals } from './reportWebVitals';
+import App from '@/app/App';
+import { ProjectProvider } from '@/app/project/ProjectContext';
+import { LeakFormProvider } from '@/features/leakForm/LeakFormContext';
+import ErrorBoundary from '@/components/ui/ErrorBoundary/ErrorBoundary';
+import { reportWebVitals } from '@/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
